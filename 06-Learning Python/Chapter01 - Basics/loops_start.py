5a699502278931df9bc1faa03aecbbbb5d20000a
ManojMargi <74328573+ManojMargi@users.noreply.github.com>
2024-11-07 18:24:51 +0000

    Update loops_start.py

--- 06-Learning Python/Chapter01 - Basics/loops_start.py
+++ 06-Learning Python/Chapter01 - Basics/loops_start.py
@@ -28,7 +28,9 @@ def main():
         print (x)
 
     # TODO: using the enumerate() function to get index 
-    
+    days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+    for i, d in enumerate(days):
+        print (i, d)
   
 if __name__ == "__main__":
     main()
