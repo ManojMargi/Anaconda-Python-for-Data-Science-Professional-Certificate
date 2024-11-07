1b8550d22e34c24a0e361e657ede3e150ecc36f7
ManojMargi <74328573+ManojMargi@users.noreply.github.com>
2024-11-07 18:55:51 +0000

    Update challenge_solution.py

--- 06-Learning Python/Chapter01 - Basics/challenge_solution.py
+++ 06-Learning Python/Chapter01 - Basics/challenge_solution.py
@@ -1,40 +1,17 @@
-# Solution to programming challenge for Learning Python course
-# LinkedIn Learning Python course by Joe Marini
-#
+# Python code​​​​​​‌‌​​​​​‌​‌‌​‌‌‌​​‌‌‌‌​​​​ below
+# Use print("messages...") to debug your solution.
+
+show_expected_result = False
+show_hints = False
 
 def is_palindrome(teststr):
-    # one way to do it: calculate the reverse of the string
-    # reversestr = ""
-    # strindx = len(teststr)-1
-    # while (strindx >= 0):
-    #     reversestr += teststr[strindx]
-    #     strindx -= 1
-
-    # if teststr == reversestr:
-    #     return True
-    # return False
-
-    # more advanced: use the slice trick to reverse the string
-    if teststr == teststr[::-1]:
-        return True
-    return False
-
-run = True
-while (run):
-    teststr = input("Enter string to test for palindrome or 'exit':")
-
-    # If the user types "exit" then quit the program
-    if teststr == "exit":
-        run = False
-        break
-
-    # convert the string to all lower case
+    # Your code goes here.
     teststr = teststr.lower()
-
-    # strip all the spaces and punctuation from the string
-    newstr = ""
+    temp = ''
     for x in teststr:
-        if x.isalnum():
-            newstr += x
-
-    print("Palindrome test:", is_palindrome(newstr))
+        if x.isalpha():
+            temp += x
+    if temp == temp[::-1]:
+        return True
+    else:
+        return False
