bacd3b48fb5ba5b455b22db68bfbebda7fbc6a5a
ManojMargi <74328573+ManojMargi@users.noreply.github.com>
2024-11-07 18:23:44 +0000

    Update loops_start.py

--- 06-Learning Python/Chapter01 - Basics/loops_start.py
+++ 06-Learning Python/Chapter01 - Basics/loops_start.py
@@ -22,7 +22,10 @@ def main():
         print (d)
 
     # TODO: use the break and continue statements
-
+    for x in range(5,10):
+        #if (x == 7): break
+        #if (x % 2 == 0): continue
+        print (x)
 
     # TODO: using the enumerate() function to get index 
     
