af04e47720a3c6bc1ad7141eb031a8790171e46d
ManojMargi <74328573+ManojMargi@users.noreply.github.com>
2024-11-08 19:45:26 +0000

    Update shell_start.py

--- 06-Learning Python/Chapter02 - Files/shell_start.py
+++ 06-Learning Python/Chapter02 - Files/shell_start.py
@@ -6,21 +6,24 @@
 import os
 from os import path
 import shutil
+from shutil import make_archive
 
 def main():
     # make a duplicate of an existing file
-    if path.exists("textfile.txt"):
+    if path.exists("textfile.txt.bak"):
         # get the path to the file in the current directory
-        src = path.realpath("textfile.txt")
+        src = path.realpath("textfile.txt.bak")
 
         # let's make a backup copy by appending "bak" to the name
-        dst = src + ".bak"
-        shutil.copy(src, dst)
+        # dst = src + ".bak"
+        # shutil.copy(src, dst)
 
         # rename the original file
-        os.rename("textfile.txt", "newfile.txt")
+        # os.rename("textfile.txt", "newfile.txt")
 
         # now put things into a ZIP archive
+        root_dir , tail = path.split(src)
+        shutil.make_archive("archive", "zip", root_dir)
 
         # more fine-grained control over ZIP files
 
