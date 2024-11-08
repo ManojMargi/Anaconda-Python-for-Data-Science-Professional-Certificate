a71e5dae18f33e8adec466ca48841f36ee31e557
ManojMargi <74328573+ManojMargi@users.noreply.github.com>
2024-11-08 20:03:44 +0000

    Update challenge_solution.py

--- 06-Learning Python/Chapter02 - Files/challenge_solution.py
+++ 06-Learning Python/Chapter02 - Files/challenge_solution.py
@@ -2,33 +2,53 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+# import os
+
+# totalbytes = 0
+
+# # get a list of all the files in the current directory
+# dirlist = os.listdir()
+# for entry in dirlist:
+#     # make sure it's a file!
+#     if os.path.isfile(entry):
+#         # add the file size to the total
+#         filesize = os.path.getsize(entry)
+#         totalbytes += filesize
+
+# # create a subdirectory called "results"
+# os.mkdir("results")
+
+# # create the output file
+# resultsfile = open("results/results.txt", "w+")
+# if resultsfile.mode == "w+":
+#     resultsfile.write("Total bytecount:" + str(totalbytes) + "\n")
+#     resultsfile.write("Files list:\n")
+#     resultsfile.write("--------------\n")
+#     # write the results into the file
+#     for entry in dirlist:
+#         if os.path.isfile(entry):
+#             # write the file name to the results ledger
+#             resultsfile.write(entry + "\n")
+
+#     # close the file when done
+#     resultsfile.close()
+
+# Python code​​​​​​‌‌​​​​​‌‌​​​‌​‌​‌‌‌‌​‌‌​‌ below
+# Use print("messages...") to debug your solution.
 import os
+from os import path
 
-totalbytes = 0
+show_expected_result = False
+show_hints = False
 
-# get a list of all the files in the current directory
-dirlist = os.listdir()
-for entry in dirlist:
-    # make sure it's a file!
-    if os.path.isfile(entry):
-        # add the file size to the total
-        filesize = os.path.getsize(entry)
-        totalbytes += filesize
+def file_info():
+    totalbytes = 0
+    folder = "deps"
 
-# create a subdirectory called "results"
-os.mkdir("results")
-
-# create the output file
-resultsfile = open("results/results.txt", "w+")
-if resultsfile.mode == "w+":
-    resultsfile.write("Total bytecount:" + str(totalbytes) + "\n")
-    resultsfile.write("Files list:\n")
-    resultsfile.write("--------------\n")
-    # write the results into the file
+    dirlist = os.listdir(folder)
     for entry in dirlist:
-        if os.path.isfile(entry):
-            # write the file name to the results ledger
-            resultsfile.write(entry + "\n")
-
-    # close the file when done
-    resultsfile.close()
+        if os.path.isfile(folder + "/" + entry) and entry.endswith(".txt"):
+            filesize = os.path.getsize(folder + "/" + entry)
+            totalbytes += filesize
+    return totalbytes
+    
