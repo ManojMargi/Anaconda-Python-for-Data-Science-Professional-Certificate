7a0fc86e8b24b930f84b03c4cb719111919f6ce9
ManojMargi <74328573+ManojMargi@users.noreply.github.com>
2024-11-10 01:39:56 +0000

    Update jsondata_start.py

--- 06-Learning Python/Chapter04 - Internet Data/jsondata_start.py
+++ 06-Learning Python/Chapter04 - Internet Data/jsondata_start.py
@@ -2,27 +2,43 @@
 # Example file for parsing and processing JSON
 # LinkedIn Learning Python course by Joe Marini
 #
-
-import urllib.request 
+import urllib.request
+from urllib.request import urlopen
+import ssl
+import json
+ssl._create_default_https_context = ssl._create_unverified_context
 
 def printResults(data):
     # Use the json module to load the string data into a dictionary
     theJSON = json.loads(data)
     
     # now we can access the contents of the JSON like any other Python object
-
+    if "title" in theJSON["metadata"]:
+        print(theJSON["metadata"]["title"])
     
     # output the number of events, plus the magnitude and each event name  
-
+    count = theJSON["metadata"]["count"]
+    print(count, "events recorded")
+    print("-----------------\n")
     
     # for each event, print the place where it occurred
-
+    for i in theJSON["features"]:
+        print(i["properties"]["place"])
+    print("-----------------\n")
 
     # print the events that only have a magnitude greater than 4
-
+    for i in theJSON["features"]:
+        if i["properties"]["mag"] >= 4.0:
+            print(i["properties"]["place"])
+    print("-----------------\n")
 
     # print only the events where at least 1 person reported feeling something
-
+    print("\nEvents that were felt:")
+    for i in theJSON["features"]:
+        feltReports = i["properties"]["felt"]
+        if feltReports != None:
+            if feltReports > 0:
+                print(i["properties"]["place"], feltReports, "times")             
   
 def main():
     # define a variable to hold the source URL
@@ -33,7 +49,11 @@ def main():
     # Open the URL and read the data
     webUrl = urllib.request.urlopen(urlData)
     print ("result code: " + str(webUrl.getcode()))
-  
+    if (webUrl.getcode() == 200):
+        data = webUrl.read()
+        printResults(data)
+    else:
+        print("Received an error from the server, cannot print results", webUrl.getcode())
 
 if __name__ == "__main__":
     main()
